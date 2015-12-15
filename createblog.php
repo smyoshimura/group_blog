908fee5fba1c881caedb874bb128eb7e209f2034
michpenn <michpenn@icloud.com>
2015-12-15 15:33:39 -0800

    query to add tags to tag table

--- createblog.php
+++ createblog.php
@@ -1,15 +1,38 @@
 <?php
-$blog = array('title'=>'', 'text'=>'', 'tags'=>'', 'owner_id'=>'', 'public'=>'');
+$blog = array('title'=>'this is the title', 'text'=>'text', 'tags'=>'tag 1, tag 2, tag 3', 'owner_id'=>'1', 'public'=>'1');
 //IF BLOG IS CREATED AND PUBLISHED RIGHT AWAY AND PUBLIC
 $query = "INSERT INTO `blogs`(`id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp`, `public`, `soft_delete`) VALUES (null,'title','text','string of tags',1,NOW(),NOW(),NOW(),1,0)";
 
 //IF BLOG IS CREATED AND USER WANTS TO PUBLISH IT LATER
 $query = "INSERT INTO `blogs`(`id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp`, `public`, `soft_delete`) VALUES (null,'title','text', 'string of tags',1,NOW(),null,NOW(),1,0)";
-    //When the user updates the post set 'id' to the id of the post
-$query = "UPDATE `blogs` SET `published_timestamp`= NOW() WHERE `id` = 2";
 
 //IF BLOG IS CREATED AND PUBLISHED RIGHT AWAY AND PRIVATE
 $query = "INSERT INTO `blogs`(`id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp`, `public`, `soft_delete`) VALUES (null,'title','text', 'string of tags',1,NOW(),NOW(),NOW(),0,0)";
 
 //IF BLOG IS CREATED AND USER WANTS TO PUBLISH IT LATER AND IT IS PRIVATE
 $query = "INSERT INTO `blogs`(`id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp`, `public`, `soft_delete`) VALUES (null,'title','text', 'string of tags',1,NOW(),null,NOW(),1,0)";
+
+function seperateTags($blog){
+?>
+<pre>
+<?php
+$tags = explode(',',$blog['tags']);
+foreach($tags as $tag){
+    $query = "INSERT INTO `tags`(`blog_id`, `owner`, `tag`) VALUES ('blog id','owner id','{$tag}')";
+    print_r($query . '<br>');
+}
+
+?>
+    </pre>
+<?
+}
+
+seperateTags($blog);
+?>
+<pre>
+<?php
+print_r($blog);
+?>
+    </pre>
+
+
