e1b203315401e89700cdd8fc49382438295c0bcb
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-12-15 15:04:40 -0800

    angular skeleton

--- js/main.js
+++ js/main.js
@@ -1,13 +1,52 @@
-var app = angular.module('blogApp', []);
+var app = angular.module("blogApp", ["ngRoute"]);
+
+app.config(function ($routeProvider){
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'login.html',
+            controller: 'loginCtrl'
+        })
+        .when('/register', {
+            templateUrl: 'register.html',
+            controller: 'registerCtrl'
+        })
+        .when('/blog', {
+            templateUrl: 'blog.html',
+            controller: 'blogCtrl'
+        })
+        .when('/blog-list', {
+            templateUrl: 'blog-list.html',
+            controller: 'blogListCtrl'
+        })
+        .when('/post', {
+            templateUrl: 'post.html',
+            controller: 'postCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+});
 
 app.service("blogService", function ($q) {
 
 });
 
-app.controller('appController', function (blogService) {
+app.controller('loginCtrl', function (blogService) {
 
 });
 
-app.controller('entryController', function (blogService) {
+app.controller('registerCtrl', function (blogService) {
+
+});
+
+app.controller('blogCtrl', function (blogService) {
+
+});
+
+app.controller('blogListCtrl', function (blogService) {
+
+});
+
+app.controller('postCtrl', function (blogService){
 
 });
