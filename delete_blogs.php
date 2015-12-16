2ef9a906a7a0228dabc116f9407e32b46771b6f0
lance9980 <lance9980@gmail.com>
2015-12-16 00:28:03 -0800

    working on login user

--- delete_blogs.php
+++ delete_blogs.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
+session_start();
 require('db_connect.php');
 
 $user_id = $_SESSION['user_id'];
@@ -9,18 +10,18 @@ $blog_ids = $_POST['blog_ids'];
 // Query to delete blog based on blog IDs selected
 $query = "DELETE FROM blogs WHERE id=$values";
 
-// Iterate through blog IDs to delete and run query
+// Iterate through blog IDs to delete while running the query
 foreach ($blog_ids as $values) {
     mysqli_query($conn, $query);
 }
 
-// Check if query operation deleted at least 1 row or output error
+// Check if query operation deleted at least 1 row, otherwise output errors
 if (mysqli_affected_rows($conn)>0) {
     $result = ['success'=>true, 'data'=>['ids'=>$blog_ids]];
     print_r(json_encode($result));
 }
 else {
-    $errors = ['DB error', 'ID error'];
+    $errors = ['DB error', 'ID error', 'Operation error'];
     $result = ['success'=>false, 'data'=>['ids'=>$blog_ids], 'errors'=>$errors];
     print_r(json_encode($result));
 }
--- login_user.php
+++ login_user.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
+session_start();
 
 $user_email = $_POST['email'];
 $user_password = $_POST['password'];
@@ -10,12 +11,11 @@ $user_password = $_POST['password'];
 // perform email & password validation against DB
 require('db_connect.php');
 $query = "SELECT email, password FROM users WHERE email=$user_email";
-if
+
 
 
 // if login success
-$token = uniqid();
-//print_r($token);
+$token = md5(uniqid(mt_rand(), true));
 $_SESSION['token'] = $token;
 
 
