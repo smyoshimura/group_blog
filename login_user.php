953b6a2291a4bb190d1a9a2b511b7306e5ee430f
lance9980 <lance9980@gmail.com>
2015-12-16 15:08:21 -0800

    working on login-user token

--- login_user.php
+++ login_user.php
@@ -6,25 +6,42 @@ session_start();
 $user_email = $_POST['email'];
 $user_password = $_POST['password'];
 
-// Need to validate email chars
-
-// perform email & password validation against DB
 require('db_connect.php');
-$query = "SELECT email, password FROM users WHERE email=$user_email";
+// get user data based on user's email address inputted
+$query = "SELECT id, email, password FROM users WHERE email='$user_email'";
+$user_data = mysqli_query($conn, $query);
 
+// if email found, assign user data to $output, validate password, assign user id to SESSION variable
+if (mysqli_num_rows($user_data)>0) {
+    while ($row = mysqli_fetch_assoc($user_data)) {
+        $output[] = $row;
+    }
+    if ($output[0]['password'] == $user_password) {
+        // correct password
+        $_SESSION['user_id'] = $output[0]['id'];
+        $result = ['success'=>1, 'user_id'=>$_SESSION['user_id']];
+        unset($output);
+    }
+    else {
+        // incorrect password
+        $result = ['success'=>0, 'error'=>'incorrect password'];
+        unset($output);
+    }
+}
+else {
+    $result = ['success'=>0, 'error'=>'email not found'];
+    unset($output);
+}
 
+// print_r($result) sends the AJAX output
+if (isset($result)) {
+    print_r($result);
+}
+else {
+    $result = ['success'=>0, 'error'=>'operation error'];
+    print_r($result);
+}
 
 // if login success
-$token = md5(uniqid(mt_rand(), true));
-$_SESSION['token'] = $token;
-
-
-
-
-
-
-
-// token test
-//$length = 10;
-//$token = bin2hex(random_compat($length));
-//print_r($token);
+//$token = md5(uniqid(mt_rand(), true));
+//$_SESSION['token'] = $token;
