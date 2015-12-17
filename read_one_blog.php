ddd903cd1c0096ea863605e55e60705306ce36a8
Sara Johnston <sarajohnston4@gmail.com>
2015-12-17 12:31:57 -0800

    read and list updated

--- read_one_blog.php
+++ read_one_blog.php
@@ -4,24 +4,28 @@ header('Access-Control-Allow-Methods: GET, POST');
 require('connect.php');
 
 if(!empty($_POST)) {
-    $info = mysqli_query($conn, $query);
+
     $output=[];
     $id=$_POST['blog_id'];
-    $token = $_SESSION['auth_token'];
+    $token =$_POST['token'];
+
     if (isset($token)){
         if($token === $_SESSION['token']){
             $query = "select `id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp` from `blogs` where `id`=$id";
         }
         else{
             $public = 1;
-            $query = "select `id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp` from `blogs` where `id`=$id `public`=$public";
+            $query = "select `id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp` from `blogs` where `id`=$id and `public`=$public";
         }
     }
+    $info = mysqli_query($conn, $query);
     if(mysqli_num_rows($info)>0){
         while($row=mysqli_fetch_assoc($info)){
-            $output[$row['owner_id']][]=$row;
+            //$output[$row['owner_id']][]=$row;
+            $output[] = $row;
         };
     };
-    print(json_encode($output));
+
+    print_r(json_encode($output));
 }
 ?>
