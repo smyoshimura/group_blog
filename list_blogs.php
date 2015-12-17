e1982edd4f3396274babec6df317cc9ab34fb635
Sara Johnston <sarajohnston4@gmail.com>
2015-12-17 15:53:06 -0800

    read and list blogs are ready for testing

--- list_blogs.php
+++ list_blogs.php
@@ -2,15 +2,22 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 require('connect.php');
-$query = "select * from `blogs`";
-$result = mysqli_query($conn, $query);
-if(mysqli_num_rows($result)>0){
-    while($row=mysqli_fetch_assoc($result)){
-        $output[]=$row;
-    };
-};
-print(json_encode($output));
-?>
-<?php
 
+if (isset($token)) {
+    if ($token === $_SESSION['token']) {
+        $query = "SELECT * FROM `blogs`";
+    } else {
+        $query = "SELECT * FROM `blogs` WHERE `public`=1";
+    }
+}
+$info = mysqli_query($conn, $query);
+if (mysqli_num_rows($info) > 0) {
+    while ($row = mysqli_fetch_assoc($info)) {
+        $output[] = $row;
+    }
+    $result = ['success' => 1, 'data' => $output];
+} else {
+    $result = ['success' => 0, 'errors' => 'No blogs exist.'];
+}
+print(json_encode($result));
 ?>
--- read_one_blog.php
+++ read_one_blog.php
@@ -4,38 +4,34 @@ header('Access-Control-Allow-Methods: GET, POST');
 require('connect.php');
 session_start();
 
-if(!empty($_POST)) {
+if (!empty($_POST)) {
 
-    $id=$_POST['blog_id'];
-    $token =$_POST['token'];
+    $id = $_POST['blog_id'];
+    $token = $_POST['token'];
 
 //Checks if token is valid, if not only a public blog entry will return
-    if (isset($token)){
-        if($token === $_SESSION['token']){
+    if (isset($token)) {
+        if ($token === $_SESSION['token']) {
             $query = "select `id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp` from `blogs` where `id`=$id";
-        }
-        else{
+        } else {
             $public = 1;
             $query = "select `id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp` from `blogs` where `id`=$id and `public`=$public";
         }
     }
     $info = mysqli_query($conn, $query);
-    if(mysqli_num_rows($info)>0){
-        while($row=mysqli_fetch_assoc($info)){
+    if (mysqli_num_rows($info) > 0) {
+        while ($row = mysqli_fetch_assoc($info)) {
             $output[] = $row;
         };
-        $result = ['success'=>1, 'data'=>$output];
-    }
-    else {
-        $result = ['success'=>0, 'data'=>[], 'error'=>'no data found'];
+        $result = ['success' => 1, 'data' => $output];
+    } else {
+        $result = ['success' => 0, 'data' => [], 'error' => 'no data found'];
     }
 }
-
 if (isset($result)) {
     print_r(json_encode($result));
-}
-else {
-    $result = ['success'=>0, 'data'=>[], 'error'=>'unknown error'];
+} else {
+    $result = ['success' => 0, 'data' => [], 'error' => 'unknown error'];
     print_r(json_encode($result));
 }
 ?>
