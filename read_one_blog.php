4d1d2e478c4178ecead46743ef5dbb29d1ad27e0
Sara Johnston <sarajohnston4@gmail.com>
2015-12-17 11:36:46 -0800

    need to test read_one_blog

--- read_one_blog.php
+++ read_one_blog.php
@@ -2,18 +2,26 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 require('connect.php');
+
 if(!empty($_POST)) {
-    $id = $_POST['blog_id'];
-    $token = $_SESSION['token'];
-    if ($token === null){
-       $public = 1;
+    $info = mysqli_query($conn, $query);
+    $output=[];
+    $id=$_POST['blog_id'];
+    $token = $_SESSION['auth_token'];
+    if (isset($token)){
+        if($token === $_SESSION['token']){
+            $query = "select `id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp` from `blogs` where `id`=$id";
+        }
+        else{
+            $public = 1;
+            $query = "select `id`, `title`, `text`, `tags`, `owner_id`, `created_timestamp`, `published_timestamp`, `edited_timestamp` from `blogs` where `id`=$id `public`=$public";
+        }
     }
-    if(isset($_SESSION['token'])){
-        $token = $_SESSION['token'];
-        $public = 0;
-    }
-    $query = "select * from `blogs` where `id`=$id `public`=$public";
-    $result = mysqli_query($conn, $query);
-    print_r($_POST);
+    if(mysqli_num_rows($info)>0){
+        while($row=mysqli_fetch_assoc($info)){
+            $output[$row['owner_id']][]=$row;
+        };
+    };
+    print(json_encode($output));
 }
 ?>
