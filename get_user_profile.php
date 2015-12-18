fcb4788f4a495c0d3eb0c9b2b76cda6eedaf1c7c
Sara Johnston <sarajohnston4@gmail.com>
2015-12-18 10:40:23 -0800

    get user profile  access done

--- get_user_profile.php
+++ get_user_profile.php
@@ -4,17 +4,25 @@ header('Access-Control-Allow-Methods: GET, POST');
 require('connect.php');
 session_start();
 if(!empty($_POST)){
-    $query ="SELECT `id`, `username`, `email`, `picture`, `timestamp` FROM `users` WHERE `id`=$id";
     $id=$_POST['user_id'];
-    $info=mysqli_query($conn, $query);
-    if(mysqli_num_rows($info)>0){
-        while($row=mysqli_fetch_assoc($info)){
-            $output[]=$row;
+    $token = $_POST['token'];
+
+    if(isset($token)){
+        if ($token === $_SESSION['token']) {
+            $query ="SELECT `id`, `username`, `email`, `picture`, `timestamp` FROM `users` WHERE `id`=$id";
+            $info=mysqli_query($conn, $query);
+            if(mysqli_num_rows($info)>0){
+                while($row=mysqli_fetch_assoc($info)){
+                    $output[]=$row;
+                }
+                $result=['success'=>1, 'data'=>$output];
+            }
+            else{
+                $result=['success'=>0, 'error'=>'No user information'];
+            }
+        } else {
+            $result=['success'=>0, 'No token access'];
         }
-        $result=['success'=>1, 'data'=>$output];
-    }
-    else{
-        $result=['success'=>0, 'error'=>'No user information'];
     }
 }
 if(isset($result)){
